0942299bf6b5904a0e5787d1e73edcf339a87ab7
Md Samaul <mdsamal843@gmail.com>
2023-06-12 17:11:44 +0600

    electrical and electronics

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,4 +1,6 @@
 import Banner from "../Banner/Banner.jsx";
+import ElectricalElectronics from "../ElectricalElectronics/ElectricalElectronics.jsx";
+import Fashion from "../Fashion/Fashion.jsx";
 import ServicesSection from "../ServicesSection/ServicesSection.jsx";
 
 const Home = () => {
@@ -6,6 +8,8 @@ const Home = () => {
         <div>
             <Banner></Banner>
             <ServicesSection></ServicesSection>
+            <Fashion></Fashion>
+            <ElectricalElectronics></ElectricalElectronics>
         </div>
     );
 };
--- src/component/BrandCard/BrandCard.jsx
+++ src/component/BrandCard/BrandCard.jsx
@@ -1,8 +1,19 @@
+import { Link } from "react-router-dom";
 
-const BrandCard = () => {
+const BrandCard = ({ image, brandName, pathBrand, subTitle, pathShopnow }) => {
     return (
-        <div>
+        <div className="card w-44 h-56 mx-2 lg:mx-0 my-2 lg:my-3 lg:w-64 lg:h-60 bg-base-100 pb-4 rounded-2xl shadow-xl">
+            <figure className="px-10 pt-10 mb-6">
+                <img src={image} className="rounded-xl" />
+            </figure>
+            <div className=" items-center text-center">
+                <Link to={pathBrand} className="font-medium text-black mt-12  duration-700 hover:text-[#c32148]">{brandName}</Link>
 
+                <p className="font-thin">{subTitle}</p>
+                <div className="card-actions flex justify-center mt-6">
+                    <Link to={pathShopnow}><button className="w-32 h-8 bg-gradient-to-r from-pink-800 to-pink-500  hover:from-pink-500 hover:to-pink-800 duration-1000 text-white rounded-full">Shop Now</button></Link>
+                </div>
+            </div>
         </div>
     );
 };
